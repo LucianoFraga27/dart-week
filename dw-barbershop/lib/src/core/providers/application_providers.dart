837312daf034143addd499fe4dbd9f738c4cd7d0
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-16 19:23:37 -0300

    user login service provider

--- dw-barbershop/lib/src/core/providers/application_providers.dart
+++ dw-barbershop/lib/src/core/providers/application_providers.dart
@@ -1,4 +1,6 @@
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:dw_barbershop/src/core/services/user_login.dart';
+import 'package:dw_barbershop/src/core/services/user_login_impl.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -11,3 +13,7 @@ RestClient restClient(RestClientRef ref) => RestClient();
 @Riverpod(keepAlive: true)
 UserRepository userRepository(UserRepositoryRef ref) =>
     UserRepositoryImpl(restClient: ref.read(restClientProvider));
+
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
