3cc023296f1b818f7a7e237f523aeca12af94077
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-16 14:47:41 -0300

    User Repository

--- dw-barbershop/lib/src/core/fp/either.dart
+++ dw-barbershop/lib/src/core/fp/either.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 sealed class Either<E extends Exception, S> {}
 
 class Failure<E extends Exception, S> extends Either<E, S> {
@@ -9,9 +8,9 @@ class Failure<E extends Exception, S> extends Either<E, S> {
   );
 }
 
-class Sucess<E extends Exception, S> extends Either<E, S> {
+class Success<E extends Exception, S> extends Either<E, S> {
   final S value;
-  Sucess(
+  Success(
     this.value,
   );
 }
