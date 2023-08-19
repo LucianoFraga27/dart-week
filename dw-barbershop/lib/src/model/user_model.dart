02d0400dd356eacb1284486280e5aeb5dfd6b2b6
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-18 22:59:51 -0300

    Model User Employee

--- dw-barbershop/lib/src/model/user_model.dart
+++ dw-barbershop/lib/src/model/user_model.dart
@@ -56,4 +56,26 @@ class UserModelEmployee extends UserModel {
     required this.workDays,
     required this.workHours,
   });
+
+  factory UserModelEmployee.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'email': String email,
+        'barbershop_id': int barbaershopId,
+        'work_days': final List workDays,
+        'work_hours': final List workHours,
+      } =>
+        UserModelEmployee(
+            id: id,
+            name: name,
+            email: email,
+            avatar: json['avatar'],
+            barbaershopId: barbaershopId,
+            workDays: workDays.cast<String>(),
+            workHours: workHours.cast<int>()),
+      _ => throw ArgumentError("Invalid Json"),
+    };
+  }
 }
