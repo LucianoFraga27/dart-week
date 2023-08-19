cc8a150fdae24c4597c2e1c692de07e3baa63574
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-19 14:49:14 -0300

    Provider

--- dw-barbershop/lib/src/core/providers/application_providers.dart
+++ dw-barbershop/lib/src/core/providers/application_providers.dart
@@ -2,7 +2,10 @@ import 'package:dw_barbershop/src/core/fp/either.dart';
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
 import 'package:dw_barbershop/src/core/services/user_login.dart';
 import 'package:dw_barbershop/src/core/services/user_login_impl.dart';
+import 'package:dw_barbershop/src/model/barbershop_model.dart';
 import 'package:dw_barbershop/src/model/user_model.dart';
+import 'package:dw_barbershop/src/repositories/barbershop/barbershop_repository.dart';
+import 'package:dw_barbershop/src/repositories/barbershop/barbershop_repository_impl.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -25,6 +28,21 @@ Future<UserModel> getMe(GetMeRef ref) async {
   final result = await ref.watch(userRepositoryProvider).me();
   return switch (result) {
     Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception
+  };
+}
+
+@Riverpod(keepAlive: true)
+BarbershopRepository barbershopRepository(BarbershopRepositoryRef ref) =>
+    BarbershopRepositoryImpl(restClient: ref.watch(restClientProvider));
+
+@Riverpod(keepAlive: true)
+Future<BarbershopModel> getMyBarberShop(GetMyBarberShopRef ref) async {
+  final userModel = await ref.watch(getMeProvider.future);
+  final barbershopRepository = ref.watch(barbershopRepositoryProvider);
+  final result = await barbershopRepository.getMyBarbershop(userModel);
+  return switch (result) {
+    Success(value: final barbershop) => barbershop,
     Failure(:final exception) => throw exception
   };
 }
--- dw-barbershop/lib/src/core/providers/application_providers.g.dart
+++ dw-barbershop/lib/src/core/providers/application_providers.g.dart
@@ -64,5 +64,36 @@ final getMeProvider = FutureProvider<UserModel>.internal(
 );
 
 typedef GetMeRef = FutureProviderRef<UserModel>;
+String _$barbershopRepositoryHash() =>
+    r'a64ad01ae6b70f8192c02a09abc33ea968315cb9';
+
+/// See also [barbershopRepository].
+@ProviderFor(barbershopRepository)
+final barbershopRepositoryProvider = Provider<BarbershopRepository>.internal(
+  barbershopRepository,
+  name: r'barbershopRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$barbershopRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef BarbershopRepositoryRef = ProviderRef<BarbershopRepository>;
+String _$getMyBarberShopHash() => r'acff482071909a9d1a71426daa9d2fa2189ad270';
+
+/// See also [getMyBarberShop].
+@ProviderFor(getMyBarberShop)
+final getMyBarberShopProvider = FutureProvider<BarbershopModel>.internal(
+  getMyBarberShop,
+  name: r'getMyBarberShopProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getMyBarberShopHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetMyBarberShopRef = FutureProviderRef<BarbershopModel>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
